aa3cbcb9bd19d7d60b7b1ef58ac3e729c5fc66f5
LaveeshkaMahadea <laveeshka94@gmail.com>
2023-01-03 20:43:40 +1100

    commit tree 45a23ff1b9e3c6c4d256bdde7f470214e091a30c

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,40 @@
 class ItemsController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   def index
-    items = Item.all
-    render json: items, include: :user
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      items = user.items
+    else
+      items = Item.all
+    end
+    render json: items, status: :ok, include: :user
   end
+
+  def show
+    item = Item.find(params[:id])
+    render json: item, status: :ok
+  end
+
+  def create
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      new_item = user.items.create(item_params)
+    else
+      new_item = Item.create(item_params)
+    end
+    render json: new_item, status: :created
+  end
+
+  private
+  
+  def item_params
+    params.permit(:name, :description, :price)
+  end
+
+  def render_not_found_response
+    render json: {error: "Resource not found"}, status: :not_found
+  end
+
 
 end
